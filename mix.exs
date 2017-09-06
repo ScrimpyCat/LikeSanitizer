9f72a489977e0fe0463d6610636014c85261d8a4
Stefan Johnson <sj@scrimpycat.io>
2017-09-06 21:01:48 +1000

    Setup docs and hex package

--- mix.exs
+++ mix.exs
@@ -4,10 +4,12 @@ defmodule LikeSanitizer.Mixfile do
     def project do
         [
             app: :like_sanitizer,
+            description: "SQL-LIKE sanitization for strings.",
             version: "0.1.0",
             elixir: "~> 1.3",
             start_permanent: Mix.env == :prod,
-            deps: deps()
+            deps: deps(),
+            package: package()
         ]
     end
 
@@ -18,6 +20,14 @@ defmodule LikeSanitizer.Mixfile do
 
     # Run "mix help deps" to learn about dependencies.
     defp deps do
-        []
+        [{ :ex_doc, "~> 0.16", only: :dev, runtime: false }]
+    end
+
+    defp package do
+        [
+            maintainers: ["Stefan Johnson"],
+            licenses: ["BSD 2-Clause"],
+            links: %{ "GitHub" => "https://github.com/ScrimpyCat/LikeSanitizer" }
+        ]
     end
 end
